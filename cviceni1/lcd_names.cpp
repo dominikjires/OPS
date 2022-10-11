b84cf7ae99d9dbe22d007135d27ed8bae6f77cb3
dominikjires <100781365+dominikjires@users.noreply.github.com>
2022-10-11 09:20:11 +0200

    macro added

--- cviceni1/lcd_names.cpp
+++ cviceni1/lcd_names.cpp
@@ -1,6 +1,6 @@
 #include "mbed.h"
 #include "LCD_DISCO_F469NI.h"
-
+#define MAINLINE (7)
 LCD_DISCO_F469NI lcd;
 
 DigitalOut led1(LED1);
@@ -17,9 +17,9 @@ int main()
       lcd.Clear(LCD_COLOR_BLUE);
       lcd.SetBackColor(LCD_COLOR_BLUE);
       lcd.SetTextColor(LCD_COLOR_WHITE);
-        wait(0.5);
+      wait(0.5);
       BSP_LCD_SetFont(&Font20);
-      lcd.DisplayStringAt(0, LINE(7), (uint8_t *)"Dominik Jires", CENTER_MODE);
+      lcd.DisplayStringAt(0, LINE(MAINLINE), (uint8_t *)"Dominik Jires", CENTER_MODE);
       wait(1);
 
       lcd.Clear(LCD_COLOR_WHITE);
@@ -27,9 +27,7 @@ int main()
       lcd.SetTextColor(LCD_COLOR_BLACK);
       wait(0.5);
       BSP_LCD_SetFont(&Font20);
-      lcd.DisplayStringAt(0, LINE(7), (uint8_t *)"Matej Paclt", CENTER_MODE);
-    
-
+      lcd.DisplayStringAt(0, LINE(MAINLINE), (uint8_t *)"Matej Paclt", CENTER_MODE);
       wait(1);
     }
 }
