7526fd3e1f6b926354e53c018c0453b79a9ffbf6
dominikjires <100781365+dominikjires@users.noreply.github.com>
2022-11-15 20:50:14 +0100

    main.cpp sleep changes

--- cviceni2a/main.cpp
+++ cviceni2a/main.cpp
@@ -7,19 +7,19 @@ Thread thread1;
 Thread thread2;
 
 void text1_thread()
-{
-    ThisThread::sleep_for(2s);
+{    
       lcd.SetTextColor(LCD_COLOR_BLACK);
       BSP_LCD_SetFont(&Font24);
-      lcd.DisplayStringAt(0, LINE(2), (uint8_t *)"Jsem vlakno 1", CENTER_MODE,);   
+      lcd.DisplayStringAt(0, LINE(2), (uint8_t *)"Jsem vlakno 1", CENTER_MODE,);
+      ThisThread::sleep_for(1s);
 }
 
 void text2_thread()
-{
-    ThisThread::sleep_for(2s);
+{    
       lcd.SetTextColor(LCD_COLOR_BLACK);
       BSP_LCD_SetFont(&Font24);
-      lcd.DisplayStringAt(0, LINE(2), (uint8_t *)"Jsem vlakno 2", CENTER_MODE);   
+      lcd.DisplayStringAt(0, LINE(2), (uint8_t *)"Jsem vlakno 2", CENTER_MODE);
+      ThisThread::sleep_for(1s);
 }
 
 
@@ -28,7 +28,9 @@ int main()
     thread1.start(text1_thread);
     thread1.join();
     lcd.Clear(LCD_COLOR_WHITE);
+    ThisThread::sleep_for(1s);
     thread2.start(text2_thread);
     thread2.join();
     lcd.Clear(LCD_COLOR_WHITE);
+    ThisThread::sleep_for(2s);
 }
